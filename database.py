64186df2936cdc7270d90e365001b03aa2c710ef
Sanjarbek <matlabovsanjar38@gmail.com>
2025-03-19 22:13:38 +0500

    new

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL: str = "postgresql://test_avtoraqam_user:7QTW5OHeKf8DdXj02GzgDOwA2ls07OXm@dpg-cvdfhajqf0us73fa0aeg-a/test_avtoraqam"
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
