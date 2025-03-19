bb9b48e76e400e3fafb7254f00bee4d2c6498a7d
Sanjarbek <matlabovsanjar38@gmail.com>
2025-03-19 22:06:02 +0500

    new

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL: str = "sqlite:///./autoplate.db"
+DATABASE_URL: str = "postgresql://test_avtoraqam_user:7QTW5OHeKf8DdXj02GzgDOwA2ls07OXm@dpg-cvdfhajqf0us73fa0aeg-a/test_avtoraqam"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
